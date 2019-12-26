7f52ada8dce0c7a4a546377cc66d3a5b981f6494
felfel <a.3lyounis@gmail.com>
2019-12-26 19:02:36 +0200

    [ADD] message and conversation migrations

---
+++ database/migrations/20191226111720_create_messages_and_conversations.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMessagesAndConversations extends Migration
+{
+    public function up()
+    {
+        $this->schema->create(
+            'messages',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('content', 1000);
+                $table->unsignedInteger('author_id');
+                $table->unsignedInteger('conversation_id');
+                $table->foreign('author_id')
+                    ->references('id')->on('users')->onDelete('cascade');
+                $table->timestamps();
+            }
+        );
+        $this->schema->create(
+            'conversations',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->timestamps();
+            }
+        );
+        $this->schema->table(
+            'messages',
+            function (Blueprint $table) {
+                $table->foreign('conversation_id')
+                    ->references('id')->on('conversations')->onDelete('cascade');
+            }
+        );
+        $this->schema->create(
+            'conversation_participants',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('conversation_id');
+                $table->unsignedInteger('user_id');
+                $table->foreign('conversation_id')->references('id')->on('conversations')->onDelete('cascade');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+                $table->unique(['conversation_id', 'user_id']);
+            }
+        );
+    }
+
+    public function down()
+    {
+        $this->schema->table(
+            'messages',
+            function (Blueprint $table) {
+                $table->dropForeign('conversation_id');
+            }
+        );
+        $this->schema->dropIfExists('conversation_participants');
+        $this->schema->dropIfExists('conversations');
+        $this->schema->dropIfExists('messages');
+    }
+}
